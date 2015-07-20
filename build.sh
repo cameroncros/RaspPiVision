90dcd8023e57855cf67db8f303b8c95e695fbcd4
cameron <cameroncros@gmail.com>
2015-07-20 19:26:38 +1000

    removed profiling from build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-g++ test.cpp vision/*.cpp -pg -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o test
+g++ test.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o test
 g++ bot.cpp robot/*.cpp robot/gertbot/*.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o bot
