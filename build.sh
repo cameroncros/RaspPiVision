89b7fdb92caf2bba50c788fa0f11d7bc32d4c186
Cameron Cross <cameroncros@gmail.com>
2015-07-22 14:58:16 +1000

    Removed Profiling -pg and added -march=native to build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-g++ test.cpp vision/*.cpp -pg -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o test
-g++ bot.cpp robot/*.cpp robot/gertbot/*.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o bot
+g++ test.cpp -march=native vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o test
+g++ bot.cpp -march=native robot/*.cpp robot/gertbot/*.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o bot
