68ffd974d274319033e9db09d8a04c5c8eabdf65
Cameron Cross <cameroncros@gmail.com>
2015-09-23 18:11:46 +1000

    Added search and destroy code (finds circles, then drives towards them with course correction until they disappear

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 g++ test.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o test
 g++ bot.cpp robot/*.cpp robot/gertbot/*.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o bot
+g++ snd.cpp robot/*.cpp robot/gertbot/*.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o snd
 
---
+++ snd.cpp
@@ -0,0 +1,91 @@
+/*
+ * snd.c - Search and Destroy - Robot spins around until it finds the shape it wants, then it goes and kills it.
+ *
+ *  Created on: 17 Jun 2015
+ *      Author: cameron
+ */
+
+#include "robot/BotController.h"
+#include "vision/ImageProcessor.h"
+#include "vision/HoughCircleDetection.h"
+#include <unistd.h>
+#include <iostream>
+#include <signal.h>
+
+static volatile int keepRunning = 1;
+
+void intHandler(int dummy) {
+    keepRunning = 0;
+}
+
+void search(cv::VideoCapture *capture, BotController *bt);
+void destroy(cv::VideoCapture *capture, BotController *bt);
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		exit(1);
+	}
+	signal(SIGINT, intHandler);
+
+	std::string arg = argv[1];
+ 	cv::VideoCapture capture(arg); //try to open string, this will attempt
+        if (!capture.isOpened()) //if this fails, try to open as a video camera
+                capture.open(atoi(arg.c_str()));
+        if (!capture.isOpened()) {
+                std::cerr << "Failed to open a video device or video file!\n" << std::endl;
+                return 1;
+        }
+
+	BotController *bt = new BotController();
+	while (keepRunning) {
+		search(&capture, bt);
+		destroy(&capture, bt);
+	}
+
+	std::cout << "Shutting down" << std::endl;
+	bt->stop();
+}
+
+void search(cv::VideoCapture *capture, BotController *bt) {
+	cv::Mat frame;
+	std::vector<Region *> *regionList;
+	ImageProcessor *ip = new HoughCircleDetection(*capture);
+	while (keepRunning) {
+		(*capture) >> frame;
+		ip->cleanRegionList();
+		regionList = ip->processFrame(frame);
+		if (regionList->size() > 1) {
+			break;
+		}
+		bt->spin(10);
+	}
+}
+
+void destroy(cv::VideoCapture *capture, BotController *bt) {
+	cv::Mat frame;
+	std::vector<Region *> *regionList = new std::vector<Region *>();
+	ImageProcessor *ip = new HoughCircleDetection(*capture);
+	while (keepRunning) {
+		bt->move(0, 1);
+		(*capture) >> frame;
+		regionList = ip->processFrame(frame);
+		if (regionList->size() == 0) {
+			bt->stop();
+			break;
+		}
+		std::sort(regionList->begin(), regionList->end(), compareBySize);
+
+		Region *largest = regionList->at(0);
+
+		if ((largest->getX-frame.cols)/frame.cols > 0.05) {
+			bt->spin(-10);
+		} else if ((largest->getX-frame.cols)/frame.cols < -0.05) {
+			bt->spin(10);
+		} else {
+			bt->move(0, 1);
+		}
+
+	}
+}
+
