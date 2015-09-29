58b81fbe77bc76ad800002704858341fb50f2d5a
Cameron Cross <cameroncros@gmail.com>
2015-09-29 16:29:05 +1000

    Updated build script to speed up compilation

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
-g++ test.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o test
-g++ bot.cpp robot/*.cpp robot/gertbot/*.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o bot
-g++ snd.cpp robot/*.cpp robot/gertbot/*.cpp vision/*.cpp -std=c++11 -lopencv_core -lopencv_highgui -lopencv_imgproc -o snd
+rm *.o test bot snd
+
+g++ -c robot/gertbot/*.cpp --std=c++11 -march=native
+g++ -c vision/*.cpp --std=c++11 -march=native
+g++ -c robot/*.cpp --std=c++11 -march=native
+
+
+g++ test.cpp *.o -std=c++11 -march=native -lopencv_core -lopencv_highgui -lopencv_imgproc -o test
+g++ bot.cpp *.o -std=c++11 -march=native -lopencv_core -lopencv_highgui -lopencv_imgproc -o bot
+g++ snd.cpp *.o -std=c++11 -march=native -lopencv_core -lopencv_highgui -lopencv_imgproc -o snd
+
+rm *.o
 
