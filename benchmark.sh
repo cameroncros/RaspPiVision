3eadf103748ef00df64d619f2401acc93f31ff16
cyberfetishist <cyberfetishist@gmail.com>
2015-10-05 19:54:04 +1100

    benchmark script updated for redBox and textures 1-3

--- benchmark.sh
+++ benchmark.sh
@@ -11,3 +11,33 @@
 ./test ./tests/bluebox.avi HSVRegionMinAlloc 1000 ./tests/blueball.txt
 ./test ./tests/bluebox.avi HoughCircleDetection 1000 ./tests/blueball.txt
 ./test ./tests/bluebox.avi BasicShapeDetection 1000 ./tests/blueball.txt
+
+./test ./tests/redBox.avi RGBBasic 1000 ./tests/blueball.txt
+./test ./tests/redBox.avi HSVBasic 1000 ./tests/blueball.txt
+./test ./tests/redBox.avi HSVRegion 1000 ./tests/blueball.txt
+./test ./tests/redBox.avi HSVRegionMinAlloc 1000 ./tests/blueball.txt
+./test ./tests/redBox.avi HoughCircleDetection 1000 ./tests/blueball.txt
+./test ./tests/redBox.avi BasicShapeDetection 1000 ./tests/blueball.txt
+
+./test ./tests/redBoxT1.avi RGBBasic 1000 ./tests/blueball.txt
+./test ./tests/redBoxT1.avi HSVBasic 1000 ./tests/blueball.txt
+./test ./tests/redBoxT1.avi HSVRegion 1000 ./tests/blueball.txt
+./test ./tests/redBoxT1.avi HSVRegionMinAlloc 1000 ./tests/blueball.txt
+./test ./tests/redBoxT1.avi HoughCircleDetection 1000 ./tests/blueball.txt
+./test ./tests/redBoxT1.avi BasicShapeDetection 1000 ./tests/blueball.txt
+
+./test ./tests/redBoxT2.avi RGBBasic 1000 ./tests/blueball.txt
+./test ./tests/redBoxT2.avi HSVBasic 1000 ./tests/blueball.txt
+./test ./tests/redBoxT2.avi HSVRegion 1000 ./tests/blueball.txt
+./test ./tests/redBoxT2.avi HSVRegionMinAlloc 1000 ./tests/blueball.txt
+./test ./tests/redBoxT2.avi HoughCircleDetection 1000 ./tests/blueball.txt
+./test ./tests/redBoxT2.avi BasicShapeDetection 1000 ./tests/blueball.txt
+
+./test ./tests/redBoxT3.avi RGBBasic 1000 ./tests/blueball.txt
+./test ./tests/redBoxT3.avi HSVBasic 1000 ./tests/blueball.txt
+./test ./tests/redBoxT3.avi HSVRegion 1000 ./tests/blueball.txt
+./test ./tests/redBoxT3.avi HSVRegionMinAlloc 1000 ./tests/blueball.txt
+./test ./tests/redBoxT3.avi HoughCircleDetection 1000 ./tests/blueball.txt
+./test ./tests/redBoxT3.avi BasicShapeDetection 1000 ./tests/blueball.txt
+
+
